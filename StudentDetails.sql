7c1dae43aea8716281ec03c6850e0fdd8d7c9b58
Chiragy03 <99682614+Chiragy03@users.noreply.github.com>
2022-11-15 19:40:21 +0530

    Create StudentDetails.sql

---
+++ StudentDetails.sql
@@ -0,0 +1,22 @@
+CREATE TABLE StudentDetails
+(
+id :int
+student_id: int,
+OtherDetails: varchar
+created_at: datetime
+created_by: varchar
+updated_at: datetime
+updated_by: varchar
+);
+
+INSERT INTO StudentDetails VALUES (1, 'M', '2022-11-15', 'ABEL' , '2022-11-15', 'ABEL');
+
+INSERT INTO StudentDetails VALUES (2, 'M', '2022-11-15', 'ABEL' , '2022-11-15', 'ABEL');
+
+INSERT INTO StudentDetails VALUES (3, 'M', '2022-11-15', 'ABEL' , '2022-11-15', 'ABEL');
+
+INSERT INTO StudentDetails VALUES (4, 'M', '2022-11-15', 'ABEL' , '2022-11-15', 'ABEL');
+
+INSERT INTO StudentDetails VALUES (5, 'M', '2022-11-15', 'ABEL' , '2022-11-15', 'ABEL');
+
+
